434be0b599bb4e21c054f739844af3b91c2b7753
AnisKhoja <anis.khojaa@gmail.com>
2021-10-11 18:27:05 -0400

    Initial commit

---
+++ Bank Queue System/TicketCounter.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TicketCounter.aspx.cs" Inherits="Bank_Queue_System.TicketCounter" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Panel ID="Panel1" runat="server" BackColor="#FFFFCC" Height="455px" Width="648px">
+                <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                &nbsp;
+                <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                &nbsp;
+                <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                <br />
+                <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Counter1" Width="165px" />
+                &nbsp;&nbsp;
+                <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Counter2" Width="165px" />
+                &nbsp;
+                <asp:Button ID="Button3" runat="server" OnClick="Button3_Click1" Text="Counter3" Width="167px" />
+                <br />
+                <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+                <br />
+                <asp:ListBox ID="ListBox1" runat="server" Height="106px" Width="223px"></asp:ListBox>
+                <br />
+                <br />
+                <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Print Ticket" Width="223px" />
+                <br />
+                <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+            </asp:Panel>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Bank Queue System/TicketCounter.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Bank_Queue_System
+{
+    public partial class TicketCounter : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //check in if session is null
+            if (Session["TokenQueue"] == null)
+            {
+                //transfering data from quue to session. to print how many number of people are ahead.
+                Queue<int> tokenQueues = new Queue<int>();
+                Session["TokenQueue"] = tokenQueues;
+            }
+            
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            //transfering queue from session into another queue
+            Queue<int> queueToken = (Queue<int>)Session["TokenQueue"];
+            //printing number of people are ahead.
+            Label2.Text = queueToken.Count.ToString();
+
+            //initializing new session to 0 if session is null
+            if (Session["ListToken"] == null)
+            {
+                Session["ListToken"] = 0;
+            }
+
+            //incrementing new session with + 1 when button is click.
+            int token = (int)Session["ListToken"] + 1;
+
+            Session["ListToken"] = token;
+            queueToken.Enqueue(token);
+            addQueueList(queueToken);
+
+            
+        }
+        void addQueueList(Queue<int> queueList)
+        {
+            ListBox1.Items.Clear();
+            foreach (int t in queueList)
+            {
+                ListBox1.Items.Add(t.ToString());
+            }
+        }
+        void AddListToCounter(TextBox textBox, int counter)
+        {
+            Queue<int> queueToken = (Queue<int>)Session["TokenQueue"];
+            if (queueToken.Count == 0)
+            {
+                textBox.Text = "No customer in line";
+
+            }
+            else
+            {
+                int tokenToBeServed = queueToken.Dequeue();
+                textBox.Text = tokenToBeServed.ToString();
+                addQueueList(queueToken);
+                Label1.Text = "Token " + tokenToBeServed + " please go to counter no " + counter;
+
+            }
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            AddListToCounter(TextBox1, 1);
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            AddListToCounter(TextBox2, 2);
+        }
+        protected void Button3_Click1(object sender, EventArgs e)
+        {
+            AddListToCounter(TextBox3, 3);
+        }
+    }
+}
